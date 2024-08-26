9d82aa5a413f256e318065b1b74c2723e19f06aa
LautaLopez98 <laueze1998@gmail.com>
2024-08-26 20:45:53 -0300

    Ajustes finales

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -72,7 +72,7 @@ export const initPassport = () =>{
         {
             clientID:config.CLIENT_ID,
             clientSecret:config.CLIENT_SECRET,
-            callbackURL:"http://localhost:8080/api/sessions/cbGitHub" || config.GITHUB_CALLBACK_URL
+            callbackURL:"http://localhost:8080/api/sessions/cbGitHub"
         },
         async(tokenAcceso, tokenRefresh, profile, done)=>{
             try {
